06fd0666ce0c09927905f07f989804f35e88cab8
papusuper <samuelbuitrago13@yourdomain.com>
2024-05-11 11:18:00 -0500

    mejore un poquito

---
+++ holamundo/Hola.js
@@ -0,0 +1,17 @@
+/*
+aqui aprendi a declarar variables 
+y alertar 
+y usar la consola del explorador 
+y el tipo de variables
+*/
+const PI=3.14
+let name ="samuel"; 
+let age =  15;
+let isBoy=true;
+console.log("my name is "+name+" age "+age+" is Boy "+isBoy );
+console.log(`my name is ${name} age ${age} is Boy ${isBoy}` );
+console.log(typeof name );
+console.log(typeof age);
+console.log(typeof isBoy);
+console.log(typeof PI);
+alert(`my name is ${name} age ${age} is Boy ${isBoy}`);
