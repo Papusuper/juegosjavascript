b4d4f0eb064fdc6147e444dd2171f373a8d0a1b1
papusuper <samuelbuitrago13@yourdomain.com>
2024-05-14 11:34:39 -0500

    ya hice la suma :)

--- holamundo/Saludo.js
+++ holamundo/Saludo.js
@@ -3,4 +3,4 @@ let saludo = prompt("what is your name","saludo");
 alert(`hola gracias ${saludo}`);
 let number1 = parseInt(prompt("digite el primer numero"));
 let number2 = parseInt(prompt("digite el segundo numero"));
-alert(`la suma es ${number1+number2}`)
+alert(`la suma es ${number1-number2}`)
---
+++ holamundo/Suma.js
@@ -0,0 +1,8 @@
+console.log("Hola,señor");
+let saludo = prompt("Escoge un nombre por favor");
+alert(`Bien ahora encoge un numero ${saludo}`);
+let number1 = parseInt(prompt("digite el primer numero por favor:)"));
+let number2 = parseInt(prompt("digite el segundo numero por favor:)"));
+alert (`La suma es ${number1+number2}`);
+let despedida = prompt("gracias, pon una calificacion sobre esta calculadora");
+console.log("Adios,señor:)");
