d0bb9ef75c0260e61d5b72f3e73f32c6dfda8540
papusuper <samuelbuitrago13@yourdomain.com>
2024-05-25 12:15:06 -0500

    expresionesOperadores matematicos y conparadores.

---
+++ holamundo/expresionesOperados.js
@@ -0,0 +1,37 @@
+//Estos son operadores matematicos.
+
+let number1 = 23;
+let text1 = "samuel";
+let bolean = true;
+let boloan2 = false;
+let number2 = 14;
+let number3 = 23;
+let number4 = 23;
+let suma = number1 + number2;
+let resta = number1 - number2;
+let multiplicacion = number1 * number2;
+let division = number1 / number2;
+let residuo = number1 % number2;
+let incremento = number1 ++;
+//operadores de comparacion.
+let igualdad = number1 === number2;
+let igualdad2 = number1 == number2;
+let igualdad3 = number3 === number4;
+let mayor = number4 > number3;
+let menor = number2 < number1;
+let mayorigual = number3 >= number4;
+let menorigual = number2 <= number1;
+console.log(suma);
+console.log(resta);
+console.log(multiplicacion);
+console.log(division);
+console.log(residuo);
+console.log(incremento);
+console.log(igualdad);
+console.log(igualdad2);
+console.log(igualdad3);
+console.log(mayor);
+console.log(menor);
+console.log(mayorigual);
+console.log(menorigual)
+
