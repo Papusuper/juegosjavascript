a51fa676850a080e76ba1f1bd2bc6c06b8391e52
papusuper <samuelbuitrago13@yourdomain.com>
2024-05-25 10:17:57 -0500

    cambios a la nueva rama

---
+++ holamundo/parrafodecolores.js
@@ -0,0 +1,14 @@
+alert("Hola,que gusto hoy vamos a escoger un color");
+let color = prompt("Escribe por favor entres estos colores:red,blue,yellow");
+ document.getElementById("escribe").style.color=color;
+ 
+
+
+
+
+
+
+
+    
+
+
