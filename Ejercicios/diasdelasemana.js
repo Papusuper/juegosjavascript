a2ee3afb2985e7cfe32a92ac88c8baffec00b122
Eduardo Carvajal <jamado@novarihealth.com>
2024-07-27 08:28:32 -0500

    swith cambio 2

--- Ejercicios/diasdelasemana.js
+++ Ejercicios/diasdelasemana.js
@@ -2,37 +2,43 @@ let dia = "semana";
 let emoji = "icono";
 alert("Hola hoy escogeremos entre los dias de la semana 🎈");
 let numero = parseInt(prompt("dijite entre 1,2,3,4,5,6,7 🙂"));
-if(numero > 0 && numero < 7){
-  if(numero === 1){
-    dia = ("hola soy lunes");
-   emoji = "😔";
-  }
-  else if(numero === 2){
-    dia = ("hola soy martes");
+
+switch (numero) {
+  case 1: 
+    dia = "hola soy lunes";
+    emoji = "😔";
+    break;
+  case 2: 
+    dia = "hola soy martes";
     emoji = "🥱";
-  }
-  else if(numero === 3){
-    dia = ("hola soy miercoles");
+    break;
+  case 3: 
+    dia = "hola soy miercoles";
     emoji = "😐";
-  }
-  else if(numero === 4){
-    dia = ("hola soy jueves");
-    emoji = "😌"; 
-  }
-  else if(numero === 5){
-    dia = ("hola soy viernes"); 
+    break;
+
+  case 4: 
+    dia = "hola soy jueves";
     emoji = "🥴";
-  }
-  else if(numero === 6){
-    dia = ("soyt sabado");
+    break;
+  
+  case 5: 
+    dia = "hola soy viernes";
+    emoji = "🥴";
+    break;
+  
+  case 6: 
+    dia = "hola soy sabado";
     emoji = "🤑";
-   }
-  else{
-    dia = ("soy domingo");
+    break;
+
+  case 7: 
+    dia = "soy domingo";
     emoji = "😎";
-  }
+    break;
+
+  default:
+    alert("no puedes usar 0 ni algo mayor que 7 🧐 genio") 
+    break;
 }
-else{
-  alert("no puedes usar 0 ni algo mayor que 7 🧐 genio") 
-} 
 document.getElementById("escribe").innerText = `lo que escogiste = ${numero} ${dia} ${emoji} `; 
