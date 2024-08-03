dfe707e72ee6bd4831c81408f5ee78614dcf1e1b
papusuper <samuelbuitrago13@gmail.com>
2024-08-03 11:04:22 -0500

    ultimo cambio de dias de loa semana

--- Ejercicios/diasdelasemana.js
+++ Ejercicios/diasdelasemana.js
@@ -1,44 +1,6 @@
 function  DiaSemana(){
-  let dianumero = parseInt(document.getElementById("dia").value)
-  let dia = "";
-  let emoji = "";
-  switch (dianumero) {
-    case 1: 
-      dia = "lunes";
-      emoji = "😔";
-      break;
-    case 2: 
-      dia = "martes";
-      emoji = "🥱";
-      break;
-    case 3: 
-      dia = "miercoles";
-      emoji = "😐";
-      break;
-
-    case 4: 
-      dia = "jueves";
-      emoji = "🥴";
-      break;
-    
-    case 5: 
-      dia = "viernes";
-      emoji = "🥴";
-      break;
-    
-    case 6: 
-      dia = "sabado";
-      emoji = "🤑";
-      break;
-
-    case 7: 
-      dia = "domingo";
-      emoji = "😎";
-      break;
-
-    default:
-      alert("no puedes usar 0 ni algo mayor que 7 🧐 genio") 
-      break;
-  }
-  document.getElementById("escribe").innerText = `semana loca y yo soy ${dia},${emoji}`;
+  let dianumero = parseInt(document.getElementById("dia").value)-1
+  let emoji = ["🧐","😎","🤑","🥴","😐","🥱","😔"];
+  let dia = ["lunes","martes","miercoles","jueves","viernes","sabado","domingo"]
+  document.getElementById("escribe").innerText = `semana loca y yo soy ${dia[dianumero]},${emoji[dianumero]}`;
 } 
