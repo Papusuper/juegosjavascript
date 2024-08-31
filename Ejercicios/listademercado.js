7e44414dffe1fb0ba1a0764a39fbee2ff94fe358
papusuper <samuelbuitrago13@gmail.com>
2024-08-31 10:51:51 -0500

    esta la clse de hoy

--- Ejercicios/listademercado.js
+++ Ejercicios/listademercado.js
@@ -1,20 +1,33 @@
 // 1 hacer una function que agrege un item a una  lista de mercado cuando el usuario aprete el boton
 // 2 obtener el valor que el usuiario digito en un input 
 // 3 agregar el valor del usuario a la lista
-// 4 mostrar los valores en una lista 
+// 4 mostrar los valores en una lista
+// se declaro un arreglo o vacio. 
 let listaDeMercado = [];
+// se obtienen los item de la etiqueta html
 let item = document.getElementById("item");
-let lista = document.getElementById("lista"); 
-function  agregarListaDeMercado(){  
+let lista = document.getElementById("lista");
+ // agrega una funcion que agregue un item a la lista del usuario.
+function  agregarListaDeMercado(){
+  // agrega el valor del input a la lista  
   listaDeMercado.push(item.value);
+  // llamr una funtion de lista de mercado
   imprimirListaDeMercado();   
 }
+// llama una funcion que imprime la lista de mercado o los objetos de ponga los usuarios 
 function imprimirListaDeMercado(){
   lista.innerHTML = "";
   listaDeMercado.forEach(function(mercado){
    let nuevoElemento = document.createElement("li");
    nuevoElemento.textContent = mercado;
    lista.appendChild(nuevoElemento);
-   console.log("Esta es la lista:" + mercado);
- });
+  });
+  // este document muestra la cantidad 
+  document.getElementById("cantidad").innerText = `la cantidad total es :${listaDeMercado.length}`;
+}
+// agegra una funcion que quite todos los elementos que allan en la lista. 
+ function quitarElementos(){
+   console.log("estoy en esta funcion");
+   listaDeMercado = [];
+   imprimirListaDeMercado();
 }
